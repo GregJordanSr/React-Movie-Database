e673882c9687140e811b37fbcdbb313a4a2874f8
Greg Jordan <gregljordan@gmail.com>
2019-07-03 20:16:33 -0400

    Built header component

--- React RMDB - Start Here/React-RMDB/src/components/App/App.js
+++ React RMDB - Start Here/React-RMDB/src/components/App/App.js
@@ -1,4 +1,14 @@
 import React from 'react';
-import Header from '../Header/Header';
+import Header from '../elements/Header/Header';
 import Home from '../Home/Home';
 
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <Home />
+        </div>
+    )
+}
+
+export default App;
