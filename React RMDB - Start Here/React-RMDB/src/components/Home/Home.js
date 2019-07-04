7ea94295f72ea4b2578e388eaca4dc94041c2d90
Greg Jordan <gregljordan@gmail.com>
2019-07-04 17:16:45 -0400

    Built out the grid for the movies thumbs and completed searching functions

--- React RMDB - Start Here/React-RMDB/src/components/Home/Home.js
+++ React RMDB - Start Here/React-RMDB/src/components/Home/Home.js
@@ -26,6 +26,23 @@ import Spinner from '../elements/Spinner/Spinner';
          this.fetchItems(endpoint);
      }
 
+     searchItems= (searchTerm) => {
+         console.log('Test =>', searchTerm);
+         let endpoint = '';
+         this.setState({ 
+             movies: [],
+             loading: true,
+             searchTerm
+         })
+         
+         if (searchTerm === '') {
+             endpoint =`${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+         } else {
+             endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${searchTerm}`;
+         }
+         this.fetchItems(endpoint);
+     }
+
      loadMoreItems = () => {
          let endpoint = '';
          this.setState({ loading: true });
@@ -44,21 +61,46 @@ import Spinner from '../elements/Spinner/Spinner';
          .then(result => {
              this.setState({
                  movies: [...this.state.movies, ...result.results],
-                 hearImage: this.state.heroImage || result.results[0],
+                 heroImage: this.state.heroImage || result.results[0],
                  loading: false,
                  currentPage: result.page,
                  totalPages: result.total_pages
              });
          })
+         .catch(error => console.error('Error', error))
      }
      
 
     render() {
         return (
            <div className="rmdb-home">
-               <HeroImage />
-               <SearchBar />
-               <FourColGrid />
+           {this.state.heroImage ? 
+           <div>
+               <HeroImage 
+               image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${this.state.heroImage.backdrop_path}`}
+               title={this.state.heroImage.original_title}
+               text={this.state.heroImage.overview}
+
+               />
+               <SearchBar callback={this.searchItems} />
+           </div> : null 
+           }
+           <div className="rmdb-home-grid">
+               <FourColGrid
+               header={this.state.searchTerm ? 'Search Result' : 'Popular Movies'}
+               loading={this.state.loading}
+               >
+               {this.state.movies.map( (element, i) => {
+                   return <MovieThumb
+                            key={i}
+                            clickable={true}
+                            image={element.poster_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${element.poster_path}` : './images/no_image.jpg'}
+                            movieId={element.id}
+                            movieName={element.original_title}
+                            />
+               })}
+               </FourColGrid>
+           </div>
                <Spinner />
                <LoadMoreBtn />
            </div>
