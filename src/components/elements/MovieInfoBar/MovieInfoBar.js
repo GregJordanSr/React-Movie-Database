baf96ec83a497b70ee504c420b336d5b44b85c14
Greg Jordan <gregljordan@gmail.com>
2019-07-05 10:48:25 -0400

    Changed repo structure

---
+++ src/components/elements/MovieInfoBar/MovieInfoBar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MovieInfoBar extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
