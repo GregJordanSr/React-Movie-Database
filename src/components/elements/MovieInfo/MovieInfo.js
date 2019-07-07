27d114b1f2b701ef6c7a40b860358585844a5eac
Greg Jordan <gregljordan@gmail.com>
2019-07-06 22:57:14 -0400

    The app is completed

--- src/components/elements/MovieInfo/MovieInfo.js
+++ src/components/elements/MovieInfo/MovieInfo.js
@@ -12,12 +12,12 @@ const MovieInfo = props => {
                 background: props.movie.backdrop_path ? `url('${IMAGE_BASE_URL}${BACKDROP_SIZE}${props.movie.backdrop_path}')` : '#000'
             }}
         >
-                <div className="rmdb-movieinfo-content">
-                    <div className="rmdb-movieinfo-thumb">
-                        <MovieThumb
-                            image={props.movie.poster_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${props.movie.poster_path}` : './images/no_image.jpg'}
-                            clickable={false}
-                    />
+            <div className="rmdb-movieinfo-content">
+                <div className="rmdb-movieinfo-thumb">
+                    <MovieThumb
+                        image={props.movie.poster_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${props.movie.poster_path}` : './images/no_image.jpg'}
+                        clickable={false}
+                    /> 
                     <div className="rmdb-movieinfo-text">
                         <h1>{props.movie.title}</h1>
                         <h3>PLOT</h3>
@@ -29,7 +29,7 @@ const MovieInfo = props => {
                         </div>
                         {props.directors.length > 1 ? <h3>DIRECTORS</h3> : <h3>DIRECTOR</h3>}
                         {props.directors.map( (element, i) => {
-                            return <p className="rmdb-director" key={i}>{element.name}</p>
+                            return <p key={i} className="rmdb-director">{element.name}</p>
                         })}
                     </div>
                     <FontAwesome className="fa-film" name="film" size="5x" />
